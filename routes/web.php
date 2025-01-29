dd30c7c662b0aeb81b373fc237a9b12cac3e6e3e
kinshuk <webgrity158@gmail.com>
2025-01-29 19:12:07 +0530

    added send mail

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,14 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\SendMail;
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
+
+Route::get('/', function () {
+    return view('send-mail');
+});
 
 Route::get('/home', [HomeController::class, 'home'])->middleware('track');
+
+// Route::get('/', [SendMail::class, 'sendMail']);
+Route::get('/send-mail', [SendMail::class, 'sendMail']);
